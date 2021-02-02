29738c94fc30bbaa42f761fa7be8bbb5c24434e8
sdethloff <mart5282@umn.edu>
2021-02-02 17:46:23 -0600

    Script for inverting matrices using cache for assignment 2

---
+++ cachematrix.R
@@ -0,0 +1,34 @@
+## Script to invert matrices using cache
+## Two functions required
+
+## Function to store matrix in cache
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## Function to invert matrix from cache
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
